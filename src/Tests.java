ba5bf0a0208b7dde730b7ac68e34d2ea7e8b32bb
Sidorcenko <m20405657@gmail.com>
2024-06-05 21:42:24 +0300

    Prezentācijas labošana un jautājumu izgudrošana

--- src/Tests.java
+++ src/Tests.java
@@ -1,4 +1,3 @@
-
 public class Tests {
 
 	public static void main(String[] args) {
@@ -6,10 +5,25 @@ public class Tests {
 		String[] jaut = {"Kas ir cikls?",
 				"Cik cikli ir kopā Javā?",
 				"Kas ir cikls ar priekšnosacījumu?",
-				};
+				"Cik reizes nostrādās cikls while?",
+				"Kur atrodas cikla nosacījums?",
+				"Kas jāraksta, lai cikls darbojas bezgalīgi?",
+				"Kurš operators tiek izmantots, lai pārtrauktu while ciklu?",
+				"Kāds būs šāda koda rezultāts? \n\nint x = 10; \nwhile (x > 0) { \nSystem.out.print(x + \" \"); \nx -= 2; \n}",
+				"Kurš operators tiek izmantots, lai turpinātu nākamās while cikla iterācijas izpildi?",
+				"Cik reizes šis kods drukā \"Hello\"? \n\nint n = 5; \nwhile (n > 0) { \nSystem.out.println(\"Hello\"); \nn--; \n}",
+				"Kas notiek, ja nosacījums while ciklā vienmēr ir patiess?",
+				"Kāda ir sintakse, kuru izmanto while cikls Java?",
+				"Kāds būs šāda koda rezultāts? \n\nint m = 10; \nwhile (m >= 0) { \nSystem.out.print(m + \" \"); \nm -= 2; \n}",
+				"Kāds būs šāda koda rezultāts? \n\nint n = 1; \nwhile (n <= 3) { \nSystem.out.print(n * n + \" \"); \nn++; \n}",
+				"Cik reizes minimāli nostrādās cikls while?"};
 		
+		String[] atb1 = {"Process, kurā kāda darbība tiek veikta vairākkārtigi","Tas ir veids, kā izveidot grafiskos elementus Java",
+				"Funkcija, kas veic matemātiskas darbības", "Šis ir atslēgvārds objektu izveidei Java"};
+		String[] atb2 = {"1","3","2","4"};
+		 
 		int x;
-
+		
 	}
 
 }
